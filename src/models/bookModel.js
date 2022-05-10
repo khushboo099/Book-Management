b2b5038a6d0b08b78aa5de5a6d44803be32ee630
ChanduKu <kuntrapakuc@gmail.com>
2022-05-10 14:52:48 +0530

    bookModel added

---
+++ src/models/bookModel.js
@@ -0,0 +1,53 @@
+const mongoose=require('mongoose')
+const { stringify } = require('querystring')
+const book=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    excerpt:{
+        type:String,
+        required:true,
+        trime:true1
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true,
+        trim:true
+    },
+    ISBN:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    category:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    subcategory:[{
+        type:String,
+        required:true,
+    }],
+    reviews:{
+        type:Number,
+        default:0,
+    },
+    deletedAt:{
+        type:Date,
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    },
+    releasedAt:{
+        type:Date,
+        default:true
+    }
+},{timestamps:true})
+
+module.exports=mongoose.model("book",book)
