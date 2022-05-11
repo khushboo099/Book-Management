628fbeb2b375a8521bb287887dbd10398e96b323
sagarsardar-uranium <sagarsardar.net@gmail.com>
2022-05-11 17:45:34 +0530

    book apis are done

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -3,10 +3,10 @@ const ObjectId = mongoose.Types.ObjectId;
 
 const reviewSchema = new mongoose.Schema(
   {
-    bookId: { type: ObjectId, required: true, ref: "Books" },
+    bookId: { type: ObjectId, required: true, ref: "book" },
 
     reviewedBy: { type: String, required: true, default: "Guest", trim: true },
-    reviewedAt: { type: Date, immutable: true, default: () => Date.now() },
+    reviewedAt: { type: Date, required: true },
     rating: { type: Number, required: true, min: 1, max: 5 },
     review: { type: String, trim: true },
     isDeleted: { type: Boolean, default: false },
