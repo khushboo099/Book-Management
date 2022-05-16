d1077f950819fe2a4f61e6e68687076b391096dc
unknown <khusboo0945@gmail.com>
2022-05-16 14:35:53 +0530

    final

--- src/route/route.js
+++ src/route/route.js
@@ -6,17 +6,21 @@ const getByBook = require("../contorllers/getByBookId")
 const updateController = require("../contorllers/updateControllers")
 const reviewController = require("../contorllers/reviewUpdate")
 const middleware = require("../middleware/auth")
+
 //-------------------user api-----------------------//
 router.post('/register',userController.createUser)
 router.post('/login',userController.loginUser)
+
 //------------------Book apis-----------------------//
 router.post('/books',middleware.authentication,middleware.authorization,bookController.bookCreation)
 router.get('/books',middleware.authentication,bookController.getAllBooks)
 router.get('/books/:bookId',middleware.authentication,getByBook.getBookById)
 router.delete('/books/:bookId',middleware.authentication,middleware.authorization,getByBook.deletedata)
 router.put('/books/:bookId',middleware.authentication,middleware.authorization,updateController.updatebook)
+
 //------------------review apis--------------------//
 router.post("/books/:bookId/review",middleware.bookCheck,reviewController.reviews)
 router.put("/books/:bookId/review/:reviewId",middleware.bookCheck,reviewController.updateReview)
 router.delete("/books/:bookId/review/:reviewId",middleware.bookCheck,reviewController.deleteReview)
+
 module.exports=router; 
